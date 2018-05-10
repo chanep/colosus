fix policy-offset formula

colosus/searcher.py
@@ -14,8 +14,8 @@ class Searcher:
 
         for i in range(iterations):
             root_state.select()
-        policy, value, move, new_state = root_state.play(self._get_temperature(root_state.position().move_count))
-        return policy, value, move, new_state
+        policy, temp_policy, value, move, new_state = root_state.play(self._get_temperature(root_state.position().move_count))
+        return policy, temp_policy, value, move, new_state
 
     def _get_temperature(self, move_count):
         if move_count <= self.config.move_count_temp0:
@@ -24,7 +24,7 @@ class Searcher:
                 index = np.random.choice(len(temp_prob), 1, p=temp_prob)[0]
                 return self.config.temp0[1][index]
             else:
-                self.config.temp0
+                return self.config.temp0
         else:
             return self.config.tempf
 

colosus/state.py
@@ -38,22 +38,26 @@ class State:
         for i in range(policy_len):
             child = self.children()[i]
             if child is not None:
-                policy[i] = abs(child.N + self.config.policy_offset) / self.N
-        policy = policy / np.sum(policy)
+                policy[i] = child.N / self.N
         return policy
 
     def play(self, temperature) -> (int, float, int, 'State'):
         policy = self.get_policy()
         if temperature < 0.1:
             move = np.argmax(policy)
+            temp_policy = np.zeros_like(policy)
+            temp_policy[move] = 1.0
         else:
-            temp_policy = self.apply_temperature(policy, temperature)
+            play_policy = np.clip(policy + (self.config.policy_offset / self.N), 0.0, None)
+            play_policy = play_policy / np.sum(play_policy)
+
+            temp_policy = self.apply_temperature(play_policy, temperature)
             move = np.random.choice(len(temp_policy), 1, p=temp_policy)[0]
         new_root_state = self.children()[move]
         new_root_state.parent = None
         self.noise = None
         value = -self.Q
-        return policy, value, move, new_root_state
+        return policy, temp_policy, value, move, new_root_state
 
     def play_static_policy(self, temperature) -> (int, float, int, 'State'):
         self.select()

colosus/tests/test_colosus_model.py
@@ -116,7 +116,7 @@ class ColosusModelTestCase(unittest.TestCase):
         colosus.build()
         # colosus.load_weights("c_1_500_1600.h5")
         # colosus.load_weights("c_4_600_1600.h5")
-        colosus.load_weights("c_45_10100_800.h5")
+        colosus.load_weights("c_30_1100_1600.h5")
 
         pos.print()
 
@@ -131,16 +131,17 @@ class ColosusModelTestCase(unittest.TestCase):
         print('')
 
         search_config = SearchConfig()
+        search_config.temp0 = 1.45
         searcher = Searcher(search_config)
         state_config = StateConfig()
         state_config.noise_factor = 0
-        state_config.policy_offset = -0.75
+        state_config.policy_offset = -0
         state = State(pos, None, None, colosus, state_config)
-        policy, value, move, new_state = searcher.search(state, 800)
+        policy, temp_policy, value, move, new_state = searcher.search(state, 800)
         print("value: " + str(value))
         print("moves prob")
         sorted_policy = self.sort_policy(policy)
-        for i in range(10):
+        for i in range(15):
             m = sorted_policy[i]
             print("{} - {}".format(m[0], m[1]))
 
@@ -148,6 +149,14 @@ class ColosusModelTestCase(unittest.TestCase):
             c = state.children()[m]
             if c is not None:
                 print("{} N: {}, W: {:.3g}, Q: {:.3g}, p:{:.3g}".format(Square.to_string(m), c.N, c.W, c.Q, c.P))
+
+        print("temp_policy")
+        sorted_temp_policy = self.sort_policy(temp_policy)
+        for i in range(15):
+            m = sorted_temp_policy[i]
+            print("{} - {}".format(m[0], m[1]))
+
+        print("sum temp_policy: " + str(np.sum(temp_policy)))
 
     def test_predict_on_batch(self):
         pos = Position()